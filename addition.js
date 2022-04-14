ea6b8acf99a9283b9652fef1773279b554f46b37
kantharaj <kantharajj46@gmail.com>
2022-04-14 11:25:07 +0530

    addition done

---
+++ addition.js
@@ -0,0 +1,6 @@
+function add() {
+var a = document.getElementById("firstno").value
+var b =  document.getElementById("secondno").value
+var result = parselnt(a) + parselnt(b)
+document.getElementById("result").value=result
+}
